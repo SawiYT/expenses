6066e5f894e612c0d1b1378343da9b8d0ea92696
ValeryR <patrykzielinski123321@gmail.com>
2026-03-14 02:06:38 +0100

    fix for build

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@
 import type { ThemeProviderProps } from 'next-themes';
 import * as React from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
-import { useRouter } from 'next/navigation';
 import { I18nProvider } from '@/lib/i18n';
 import { Toaster } from '@/components/ui/sonner';
 import { SessionProvider } from 'next-auth/react';
